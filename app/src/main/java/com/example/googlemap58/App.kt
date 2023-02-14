f7c3d5c3286f33b6b2ec1f76f09cca0a0f8218b0
Arik1021 <aaa100107aaa@gmail.com>
2023-02-14 22:46:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/googlemap58/App.kt
@@ -0,0 +1,7 @@
+package com.example.googlemap58
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App :Application()
---
+++ app/src/main/java/com/example/googlemap58/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.googlemap58
+
+object Constants {
+    const val CHANNEL_ID ="12345667"
+    const val NOTIFICATION_ID=989898
+}
